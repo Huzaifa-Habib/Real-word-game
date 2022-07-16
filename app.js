353f08bd96dd93977ac0b6e8a98c2315036d26c7
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-07-17 01:53:03 +0500

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,324 @@
-
-// console.log(Dictonery)
-
 var randomLetters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var _1;
+var _2;
+var _3;
+var _4;
+var _5;
+var _6;
+var _7;
+var _8;
+var _9;
+var _10;
+var _11;
+var _12;
+var _13;
+var _14;
+var _15;
+var _16;
+var _17;
+var _18;
+var _19;
+var _20;
+var _21;
+var _22;
+var _23;
+var _24;
+var _25;
+var _26;
+
+var hide= document.getElementById("hide")
+hide.style.display="none"
+
+
+// For Generating random letters at each box
 function one(){
-    var show = randomLetters [Math.floor(Math.random()*randomLetters.length)]
-    var one = document.getElementById("one").innerText=show
+    hide.style.display="block"
+
+
+
+    var show = randomLetters [Math.floor(Math.random() * 26 )]
+     _1 = document.getElementById("one").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _2 =document.getElementById("two").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _3 = document.getElementById("3").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _4 = document.getElementById("4").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _5 = document.getElementById("5").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _6 = document.getElementById("6").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _7 = document.getElementById("7").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _8 =document.getElementById("8").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _9 =document.getElementById("9").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _10 = document.getElementById("10").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _11 = document.getElementById("11").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _12 = document.getElementById("12").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _13 = document.getElementById("13").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _14 =  document.getElementById("14").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _15 =  document.getElementById("15").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _16 = document.getElementById("16").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _17 = document.getElementById("17").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _18 =  document.getElementById("18").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _19 =  document.getElementById("19").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _20 = document.getElementById("20").innerText=show
+
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _21 = document.getElementById("21").innerText=show
+
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _22 =  document.getElementById("22").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _23 =  document.getElementById("23").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _24 =  document.getElementById("24").innerText=show
+
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _25 = document.getElementById("25").innerText=show
+
+     show = randomLetters [Math.floor(Math.random() * 26 )]
+     _26 =  document.getElementById("26").innerText=show
 
 }
+
+// End of generating letters
+
+
+// Get value of each box in input
+function valueButton1 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_1
+
+    
+}
+
+function valueButton2 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_2
+
+}
+
+function valueButton3 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_3
+
+}
+
+function valueButton4 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_4
+
+}
+function valueButton5 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_5
+
+}
+function valueButton6 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_6
+
+}
+function valueButton7 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_7
+
+}
+function valueButton8 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_8
+
+}
+function valueButton9 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_9
+
+}
+function valueButton10 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_10
+
+}
+function valueButton11 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_11
+
+}
+function valueButton12 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_12
+
+}
+function valueButton13 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_13
+
+}
+function valueButton14 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_14
+
+}
+function valueButton15 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_15
+
+}
+function valueButton16 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_16
+
+}
+function valueButton17 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_17
+
+}
+function valueButton18 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_18
+
+}
+function valueButton19 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_19
+
+}
+function valueButton20 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_20
+
+}
+function valueButton21 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_21
+
+}
+function valueButton22 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_22
+
+}
+function valueButton23 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_23
+
+}
+function valueButton24 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_24
+
+}
+function valueButton25 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_25
+
+}
+function valueButton26 () {
+    var calculatedResult=document.getElementById("screen")
+    calculatedResult.value +=_26
+
+}
+
+
+var input =document.getElementById("screen").value;
+var points = 1
+
+function submit () {
+    var input =document.getElementById("screen").value;
+    input=input.toLowerCase();
+
+    for (var k = 0; k < Dictonery.length; k++) {
+        if (input === Dictonery[k]){
+            document.getElementById("point").innerText=points++
+        }
+
+        // else {
+        //     input !== Dictonery[k]
+        //     console.log("Not Correct")
+        //     break;
+        // }
+    }
+
+
+    if (input.length <= 1 || input.length > 20) {
+        alert("Word should be greater than 1 and under 15 characters")
+    }
+
+ 
+}
+
+
+function del () {
+    var text=document.getElementById("screen").value
+    document.getElementById("screen").value=text.substring(0,text.length-1)
+}
+
+function reset () {
+    var text=document.getElementById("screen").value
+    document.getElementById("screen").value=""
+
+}
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
